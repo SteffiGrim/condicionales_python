fb05b4938b64d9a4281b94dfc800e91ff6e207fc
SteffiGrim <svgrimaldi@gmail.com>
2021-10-21 04:57:26 -0300

    ejercicios practica realizados

--- ejercicios_practica/ejercicio_1.py
+++ ejercicios_practica/ejercicio_1.py
@@ -13,18 +13,36 @@
 # Ingrese dos números cualesquiera y realice las sigueintes
 # comparaciones entre ellos
 numero_1 = int(input('Ingrese el primer número:\n'))
-
 numero_2 = int(input('Ingrese el segundo número:\n'))
 
 # Compare cual de los dos números es mayor
 # Imprima en pantalla según corresponda
+if (numero_1 > numero_2):
+    print(f'{numero_1} es mayor a {numero_2}')
+else:
+    print(f'{numero_2} es mayor a {numero_1}')
 
 # Verifique si el numero_1 positivo, negativo o cero
 # Imprima el resultado en cada caso
+if (numero_1 > 0):
+    print("el numero" ,numero_1, "es positivo")
+elif (numero_1 < 0):    
+    print("el numero ", numero_1," es negativo")
+elif (numero_1 == 0):
+    print("el numero ", numero_1," es 0")
 
 # Verifique si el numero_1 es mayor a 0 y menor a 100
 # Imprima en pantalla si se cumple o no la condición
+if (numero_1 > 0 and numero_1 < 100):
+    print("el numero es mayor a 0 y menor que 100")
 
 # Verifique si el numero_1 es menor a 10 o el numero_2
 # es mayor a -2
 # Imprima en pantalla si se cumple o no la condición
+if (numero_1 < 10 or numero_2 > -2):
+    print("el numero_1 es menor a 10 o el numero_2 es mayor a -2")
+
+
+
+#RECORDATORIO PARA MI: EN VEZ DE INTERPOLAR SIEMPRE (F'-----{VARIABLE}.....)
+#PREFERIBLE PONER ("---", LA VARIABLE, ".....") MAS FACIL...
--- ejercicios_practica/ejercicio_4.py
+++ ejercicios_practica/ejercicio_4.py
@@ -17,12 +17,24 @@ texto_2 = '7'
 # se utiliza el operador mayor o menor con Strings (textos)
 # Imprima en pantalla según corresponda
 
+if(texto_1 < texto_2):
+    print("el texto_2 es mayor")
+else:
+    print("el texto_1 es mayor")
+
 # 2-Transforma esas variables tipo texto en variables numéricas con (int)
 # y almacénalas en nuevas variables.
 # Compare las nuevas variables para ver cual es mayor o menor
 # utilizando los operadores correspondientes
 # ¿Cuál de las nuevas variables es mayor?
 # Imprima en pantalla según corresponda
+
+texto_1 = int(texto_1)
+texto_2 = int(texto_2)
+if(texto_1 < texto_2):
+    print("el texto_2 es mayor")
+else:
+    print("el texto_1 es mayor")
 
 # Para pensar!
 # ¿Por qué cree que texto_2 es mayor a texto_1?
@@ -31,4 +43,9 @@ texto_2 = '7'
 # variables numéricas, cierto? ¿Por qué creen que es así?
 # Esta pregunta estará repetida en el Campus para que puedan
 # responder.
+
+#RESPUESTA: no busqué en google, recuerdo que la primera clase hablaron acerca del programa y que por cada letra habia una posición establecida con números. 
+#es decir que independientemente de los caracteres de texto que haya, cada letra, cada numero, cada simbolo del teclado 
+#tiene un valor asignado y se contará (para mayor o menor) en base a lo ya establecido en la realización de ese programa.
+#Siete y Cinco tienen la misma cantidad de letras en string. 
 # NOTA: La respuesta no se encuentra en el apunte, sino en Google ;)
